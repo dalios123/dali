c6f8b65d3f0c5a95ab12d00b965e323830085ae6
MedAmineOueslati <104275110+MedAmineOueslati@users.noreply.github.com>
2023-03-27 11:25:44 +0100

    Login

--- projet/api/views.py
+++ projet/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['age'] = user.age
+        token['is_staff'] = user.is_staff
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+    return Response(routes)
--- projet/frontend/src/App.js
+++ projet/frontend/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import Header from './components/Header'
 
 function App() {
   return (
-      <h1>Hello !!!!</h1>
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Header/>
+          <Routes>
+            <Route element = <HomePage/> path="/" exact/>
+            <Route element = <LoginPage/> path="/login"/>
+          </Routes>
+        </AuthProvider>
+      </Router>
+
+    </div>
   );
 }
 
